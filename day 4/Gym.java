bd8a93ffb194bada8e13ee1425ff13859b89c4e8
kethiyan <kithiyonkeralmark@gmail.com>
2025-12-20 19:10:42 +0530

    home work

---
+++ day 4/Gym.java
@@ -0,0 +1,7 @@
+class Gym
+{
+    public static void main(String[] args) {
+        System.out.println("Naalaikku day 1...Scratch irunthu start pandren");
+        
+    }
+}
---
+++ day 6/Variables.java
@@ -0,0 +1,35 @@
+class Variables
+{
+	public static void main(String[] args)
+	{
+		byte B=10;
+		Short S=200;
+		long L=100000l;
+		float F=5.5f;
+		double D=123.456;
+		char C='Z';
+		boolean ok =false;
+		
+		
+		System.out.println("B"+B);
+		System.out.println("S"+S);
+		System.out.println("F"+F);
+		System.out.println("D"+D);
+		System.out.println("C"+C);
+		System.out.println("ok"+ok);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
